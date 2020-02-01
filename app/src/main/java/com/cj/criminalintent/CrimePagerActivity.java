909e4ca052ae74aa048cdeec42fc94ce6c56a61f
chenjun <437456560@qq.com>
2020-02-01 09:12:40 +0800

    1.恢复 CrimeFragment 的边距; 2.添加 Jump to First 按钮和 Jump to Last 按钮.

--- app/src/main/java/com/cj/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/cj/criminalintent/CrimePagerActivity.java
@@ -3,6 +3,8 @@ package com.cj.criminalintent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,6 +21,8 @@ public class CrimePagerActivity extends AppCompatActivity {
     private static final String EXTRA_CRIME_ID = "com.cj.criminalintent.crime_id";
 
     private ViewPager mViewPager;
+    private Button mJump2startButton;
+    private Button mJump2endButton;
     private List<Crime> mCrimes;
 
     public static Intent newIntent(Context packageContext, UUID crimeId) {
@@ -35,6 +39,8 @@ public class CrimePagerActivity extends AppCompatActivity {
         UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
 
         mViewPager = findViewById(R.id.crime_view_pager);
+        mJump2startButton = findViewById(R.id.jumpToStart);
+        mJump2endButton = findViewById(R.id.jumpToEnd);
 
         mCrimes = CrimeLab.get(this).getCrimes();
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -51,11 +57,53 @@ public class CrimePagerActivity extends AppCompatActivity {
                 return mCrimes.size();
             }
         });
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if (position == 0) {
+                    mJump2startButton.setVisibility(View.INVISIBLE);
+                    mJump2endButton.setVisibility(View.VISIBLE);
+                } else if (position + 1 == mCrimes.size()) {
+                    mJump2startButton.setVisibility(View.VISIBLE);
+                    mJump2endButton.setVisibility(View.INVISIBLE);
+                } else {
+                    mJump2startButton.setVisibility(View.VISIBLE);
+                    mJump2endButton.setVisibility(View.VISIBLE);
+                }
+            }
 
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        int crimeIndex = 0;
         for (int i = 0; i < mCrimes.size(); ++i)
             if (mCrimes.get(i).getId().equals(crimeId)) {
-                mViewPager.setCurrentItem(i);
+                crimeIndex = i;
                 break;
             }
+
+        mViewPager.setCurrentItem(crimeIndex);
+
+        mJump2startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(0);
+            }
+        });
+
+        mJump2endButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(mCrimes.size() - 1);
+            }
+        });
     }
 }
