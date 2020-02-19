0a4f4f49c4f70cb45dd4b39be9db1f6dbeaf04f7
chenjun <437456560@qq.com>
2020-02-19 18:51:18 +0800

    第15章 隐式Intent

--- app/src/main/java/com/cj/criminalintent/database/CrimeCursorWrapper.java
+++ app/src/main/java/com/cj/criminalintent/database/CrimeCursorWrapper.java
@@ -4,6 +4,7 @@ import android.database.Cursor;
 import android.database.CursorWrapper;
 
 import com.cj.criminalintent.Crime;
+import com.cj.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 import java.util.Date;
 import java.util.UUID;
@@ -19,15 +20,17 @@ public class CrimeCursorWrapper extends CursorWrapper {
     }
 
     public Crime getCrime() {
-        String uuidString = getString(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.UUID));
-        String title = getString(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.TITLE));
-        long date = getLong(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.DATE));
-        int isSolved = getInt(getColumnIndex(CrimeDbSchema.CrimeTable.Cols.SOLVED));
+        String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+        String suspect = getString(getColumnIndex(CrimeTable.Cols.SUSPECT));
 
         Crime crime = new Crime(UUID.fromString(uuidString));
         crime.setTitle(title);
         crime.setDate(new Date(date));
         crime.setSolved(isSolved != 0);
+        crime.setSuspect(suspect);
 
         return crime;
     }
